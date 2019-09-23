f0f605af2102c80a65b90b8636966dc28c34c46f
Victor Chen <36187875+Scarzard@users.noreply.github.com>
2019-09-23 21:52:58 +0200

    Removed old PCG and more ImGui tests

--- TBD_Engine/ModuleEngineUI.cpp
+++ TBD_Engine/ModuleEngineUI.cpp
@@ -80,12 +80,17 @@ update_status ModuleEngineUI::Update(float dt)
 	if (main_window)
 	{
 		ImGui::Begin("Main window");
-		if (ImGui::Button("Open Demo window 1"))
+		/*if (ImGui::Button("Open Demo window 1"))
 			show_demo_window = true;
 		if (ImGui::Button("Open Demo window 2"))
 			show_another_window = true;
 		if (ImGui::Button("Open RNG window"))
-			test_rng_window = true;
+			test_rng_window = true;*/
+		
+		ImGui::Checkbox("Toggle demo window 1", &show_demo_window);
+		ImGui::Checkbox("Toggle demo window 2", &show_another_window);
+		ImGui::Checkbox("Toggle number generator window", &test_rng_window);
+
 		ImGui::End();
 	}
 	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
